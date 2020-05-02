c021e6c785ea3655d17652e25aed7e642702f685
gerruda <53830200+gerruda@users.noreply.github.com>
2020-05-02 16:00:44 +0300

    Update main1.py

--- main1.py
+++ main1.py
@@ -212,10 +212,27 @@ def feederek():
 
 #feederek()
 #bot.polling(none_stop=True)
-while True:
-	try:
-		bot.polling(none_stop=True)
-	except: 
-		print('BOLT')
+#while True:
+#	try:
+#		bot.polling(none_stop=True)
+#	except: 
+#		print('BOLT')
 #		logging.error('error: {}'.format(sys.exc_info()[0]))
-		time.sleep(5)
+#		time.sleep(5)
+class WebhookServer(object):
+    # index равнозначно /, т.к. отсутствию части после ip-адреса (грубо говоря)
+    @cherrypy.expose
+    def index(self):
+        length = int(cherrypy.request.headers['content-length'])
+        json_string = cherrypy.request.body.read(length).decode("utf-8")
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+
+if __name__ == '__main__':
+    cherrypy.config.update({
+        'server.socket_host': '127.0.0.1',
+        'server.socket_port': 7772,
+        'engine.autoreload.on': False
+    })
+    cherrypy.quickstart(WebhookServer(), '/', {'/': {}})
